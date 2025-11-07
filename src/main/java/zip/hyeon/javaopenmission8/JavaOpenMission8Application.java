fd30200204c7255b876ce8c5d9cab761608a30a5
hyeonZIP <aminez4466@gmail.com>
2025-11-07 18:23:04 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/zip/hyeon/javaopenmission8/JavaOpenMission8Application.java
@@ -0,0 +1,11 @@
+package zip.hyeon.javaopenmission8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOpenMission8Application {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaOpenMission8Application.class, args);
+    }
+}
