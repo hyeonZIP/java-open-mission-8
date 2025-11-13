49f2078903e7dd13876490a69ac36e489ca078e8
hyeonZIP <aminez4466@gmail.com>
2025-11-14 05:35:48 +0900

    feat: 로그아웃 설정 추가

--- src/main/java/zip/hyeon/javaopenmission8/adapter/security/SecurityConfig.java
+++ src/main/java/zip/hyeon/javaopenmission8/adapter/security/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.session.FindByIndexNameSessionRepository;
 import org.springframework.session.Session;
 import org.springframework.session.security.SpringSessionBackedSessionRegistry;
-import zip.hyeon.javaopenmission8.adapter.security.handler.CustomFailureHandler;
 import zip.hyeon.javaopenmission8.adapter.security.handler.CustomSuccessHandler;
 import zip.hyeon.javaopenmission8.adapter.security.service.CustomOAuth2UserService;
 
@@ -22,33 +21,41 @@ public class SecurityConfig<S extends Session> {
     private final CustomOAuth2UserService customOAuth2UserService;
     private final FindByIndexNameSessionRepository<S> sessionRepository;
     private final CustomSuccessHandler customSuccessHandler;
-    private final CustomFailureHandler customFailureHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+        http// 비활성화 설정
                 .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .logout(AbstractHttpConfigurer::disable);
+                .httpBasic(AbstractHttpConfigurer::disable);
         http// 세션 설정
                 .sessionManagement(sessionManagement -> sessionManagement
-                        .invalidSessionUrl("/invalid")
+                        .invalidSessionUrl("/home?sessionError=invalid")
                         .maximumSessions(1)
                         .maxSessionsPreventsLogin(false)
-                        .expiredUrl("/expired")
-                        .sessionRegistry(sessionRegistry())
-                );
+                        .expiredUrl("/home?sessionError=expired")
+                        .sessionRegistry(sessionRegistry()));
         http// OAuth2 로그인
                 .oauth2Login(oauth2 -> oauth2
                         .loginPage("/login")
                         .authorizationEndpoint(authorization -> authorization.baseUri("/login"))
                         .redirectionEndpoint(redirection -> redirection.baseUri("/login/code/{registrationId}"))
                         .userInfoEndpoint(userinfo -> userinfo.userService(customOAuth2UserService))
-                        .failureHandler(customFailureHandler)
-                        .successHandler(customSuccessHandler));
+                        .successHandler(customSuccessHandler)
+                        .permitAll());
+        http// 로그아웃
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutRequestMatcher(request -> request.getMethod().equals("GET") &&
+                                request.getRequestURI().equals("/logout"))
+                        .logoutSuccessUrl("/home")
+                        .deleteCookies("JSESSIONID")
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .permitAll());
+
         http// 인가 설정
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/login", "/home").permitAll()
+                        .requestMatchers("/", "/home/**", "/css/**").permitAll()
                         .anyRequest().authenticated());
 
         return http.build();
